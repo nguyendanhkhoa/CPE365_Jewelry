2334ec761881c1fff37f088f19577193e81c2970
Khoa Nguyen 1 <knguy202@calpoly.edu>
2017-03-07 12:48:38 -0800

    Customer registration page beginning

---
+++ src/CustomerRegistrationPage.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by khoanguyen1 on 3/7/17.
+ * needs to have execute update
+ *
+ */
+public class CustomerRegistrationPage {
+    private JFrame f;
+    public CustomerRegistrationPage() {
+        f = new JFrame();//creating instance of JFrame
+        f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        JPanel top = new JPanel();
+        top.setLayout(new BoxLayout(top, BoxLayout.PAGE_AXIS));
+        top.add(new JLabel("Customer Registration Page\n"){{
+            setForeground(Color.BLACK);
+        }});
+        top.add( new JLabel("What is your name"){{
+            setForeground(Color.BLACK);
+        }});
+        top.add( new JTextField(10) );
+        f.add( top );
+
+//        f.setSize(1400,800);//400 width and 500 height
+        f.pack();
+
+        f.setVisible(true);//making the frame visible
+    }
+
+    public void closeFrame(){
+        f.setVisible(false);
+        f.dispose();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+	public static void main(String[] args) throws InterruptedException {
+		CustomerRegistrationPage crp = new CustomerRegistrationPage();
+		Thread.sleep(5000); //Sleep for 5 seconds
+		crp.closeFrame(); // Close the registration window!
 	}
 
 }
