5cd992c3e3eb322f5c85bf935ec190d859fb84cb
Erica Dorn <Erica@pcp073480pcs.wireless.calpoly.edu>
2017-03-11 13:32:27 -0800

    main stuff

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		//CustomerRegistrationPage crp = new CustomerRegistrationPage();
 		//LandingPage lp = new LandingPage();
-		//new LandingPage();
-//		new HomePage();
+		new LandingPage();
+		//new HomePage();
 		//crp.closeFrame(); // Close the registration window!
 		CustomerRegistrationPage crp = new CustomerRegistrationPage();
 	}
